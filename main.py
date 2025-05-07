23bc9041527b5c414bb21b9c585d8a5493302bab
George Papadopoulos <kgpapadopoulos@proton.me>
2025-05-07 22:00:27 +0300

    Improve the main.py, Adds id, created day.

--- main.py
+++ main.py
@@ -1,26 +1,39 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
-
+import datetime
 app = FastAPI()
-
+count =1 
+data_list = []
 
 class Data(BaseModel):
     title: str
     description: str | None = None
     completed: bool
 
-data_list = []
-
 @app.post("/data")
 def create_data(data: Data):
+    global count
 
-    data_list.append(data)
+    new_data = data.dict()
+    new_data["id"] = count
+    new_data["date"] = datetime.datetime.utcnow().isoformat()
+
+    data_list.append(new_data)
+    count +=1
 
     return {
         "message": "Your data is received",
-        "data": data
+        "data": new_data
     }
 
 @app.get("/data")
 def bring_data():
     return data_list
+
+
+@app.get("/data/{id}")
+def brind_id_data(id: int):
+    for data in data_list:
+        if data["id"] == id:
+            return data
+    return {"error": "Data not found"}
